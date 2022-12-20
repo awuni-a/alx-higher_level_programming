3b92998aae5979ceed7280cceb7d4a9d1cad665f
awuni-a <awuni.alexander@gmail.com>
2022-12-20 13:09:10 +0000

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Square class definition"""
 
 
 class Square:
-    """
-    Class that defines properties of square by: (based on 0-square.py).
+    """Represents a square
     Attributes:
-        size: size of a square (1 side).
+        __size (int): size of a side of the square
     """
     def __init__(self, size):
-        """Creates new instances of square (1 side).
+        """Initializes a square
         Args:
-            size: size of the square.
+            size (int): size of a side of the square
+        Returns: None
         """
         self.__size = size 
